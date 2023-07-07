328216d1488faf8f57ec4e786eb42981ff5e8e0f
M Zharfan Wiranata <zhargorist10@gmail.com>
2023-07-07 16:22:15 +0800

    Update add_user.sh

--- ubuntu/add_user.sh
+++ ubuntu/add_user.sh
@@ -1,4 +1,16 @@
 echo "Add USER ..."
-username ="kagok"
-password ="kagokpass"
-echo $username:$password 
+# username ="kagok"
+# password ="kagokpass"
+# echo $username:$password 
+a=10
+b=20
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+fi
+
+if [ $a != $b ]
+then
+   echo "a is not equal to b"
+fi
